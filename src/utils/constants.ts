ec36ebd5e03098d1c1830aeb2f11d90e6ecb31c7
rowan.wu <>
2023-11-15 11:22:40 +0800

    修改gpt4为 gpt-4-1106-preview

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const GPT_35_TURBO = "gpt-3.5-turbo";
-export const GPT_4 = "gpt-4";
+export const GPT_4 = "gpt-4-1106-preview";
 export const GPT_MODEL_NAMES = [GPT_35_TURBO, GPT_4];
 
 export const DEFAULT_MAX_LOOPS_FREE = 3;
